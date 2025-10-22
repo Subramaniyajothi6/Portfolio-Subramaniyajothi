cf80a9a8fea70a945e13e966576c985073d07f09
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2025-10-23 00:28:23 +0530

    Fix: Correct Frameworks component import path

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -3,7 +3,7 @@ import Card from "../components/Card";
 import { Globe } from "../components/globe";
 import CopyEmailButton from "../components/CopyEmailButton";
 import SkillsProgress from "../components/SkillsProgress";
-import { Frameworks } from "../components/FrameWorks";
+import { Frameworks } from "../components/Frameworks";
 
 const About = () => {
   const grid2Container = useRef();
